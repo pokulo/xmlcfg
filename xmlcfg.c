06b3310d90023ff9ef53aa82398e3946f0f0f4cd
Heinrich <s2868288@mail.zih.tu-dresden.de>
2012-06-30 01:30:37 +0200

    s before described

--- xmlcfg.c
+++ xmlcfg.c
@@ -38,10 +38,29 @@ int joystick_load_config(const char filename[]){
 		return EXIT_FAILURE;
 	}
 
-	scew_element *MENU_DOWN = scew_element_by_name(scew_tree_root(tree),"MENU_DOWN");
+	scew_element *elem;
 
-	printf("KEY:%s \n", scew_element_name(MENU_DOWN));
-	printf("VAL:%s \n", scew_attribute_value(scew_element_attribute_by_name(MENU_DOWN,"key")));
+	enum joystick_key get_joystick_key(const char joy_node_name[MAX_STRING_LENGTH]){
+		for (int i = 0; i < JOY_COUNT; ++i) {
+			if (!strcmp(joy_loockup[i].str,joy_node_name)){
+				return joy_loockup[i].key;
+			}
+		}
+		return JOY_KEY_NOT_DEFINED;
+	}
+
+	for (int i = 0; i < USERSLOT_BOOL_COUNT; ++i) {
+		elem = scew_element_by_name(scew_tree_root(tree),userslot_bool_loockup[i].str);
+		if (elem)
+			userslot_bool_loockup[i].joy_key = get_joystick_key(scew_attribute_value(scew_element_attribute_by_name(elem,"key")));
+		else
+			return 1;//ERROR WRONG XML: Userslot userslot_bool_loockup[i].str not found
+	}
+
+
+
+//	printf("KEY:%s \n", scew_element_name(MENU_DOWN));
+//	printf("VAL:%s \n", scew_attribute_value(scew_element_attribute_by_name(MENU_DOWN,"key")));
 
 	/* Free every SCEW variable */
 	scew_reader_free(reader);
--- xmlcfg.h
+++ xmlcfg.h
@@ -10,7 +10,57 @@
 
 #include <scew/scew.h>
 #include <stdio.h>
+#define MAX_STRING_LENGTH
+//-------------------------------------joystick--------------------------------------------------
+enum joystick_key{
+	JOY_KEY_NOT_DEFINED,
+	Joy_X,
+	Joy_Y,
+	Joy_x,
+	Joy_y,
+	Joy_DC_U,
+	Joy_DC_D,
+	Joy_DC_L,
+	Joy_DC_R,
+	JOY_COUNT
+};
+typedef struct {
+	enum joystick_key key;
+	char str[63];
+}joystick_key_t;
 
+joystick_key_t joy_loockup[JOY_COUNT] = {
+		{Joy_X,"Joy_X"},
+		{Joy_Y,"Joy_Y"},
+		{Joy_x,"Joy_x"},
+		{Joy_y,"Joy_y"},
+		{Joy_DC_U,"Joy_DC_U"},
+		{Joy_DC_D,"Joy_DC_D"},
+		{Joy_DC_L,"Joy_DC_L"},
+		{Joy_DC_R,"Joy_DC_R"}
+};
+//-------------------------------------userslots--------------------------------------------------
+enum userslot_bool_key{
+	MENU_UP,
+	MENU_DOWN,
+	MENU_LEFT,
+	MENU_RIGHT,
+	USERSLOT_BOOL_COUNT
+};
+typedef struct {
+	enum userslot_bool_key key;
+	enum joystick_key joy_key;
+	char str[MAX_STRING_LENGTH];
+}userslot_bool_key_t;
+
+userslot_bool_key_t userslot_bool_loockup[USERSLOT_BOOL_COUNT] = {
+		{MENU_UP,JOY_KEY_NOT_DEFINED,"MENU_UP"},
+		{MENU_DOWN,JOY_KEY_NOT_DEFINED,"MENU_DOWN"},
+		{MENU_LEFT,JOY_KEY_NOT_DEFINED,"MENU_LEFT"},
+		{MENU_RIGHT,JOY_KEY_NOT_DEFINED,"MENU_RIGHT"}
+};
+
+//-------------------------------------funkctions--------------------------------------------------
 int joystick_load_config(const char filename[]);
 
 int main(int argc, char *argv[]) {
